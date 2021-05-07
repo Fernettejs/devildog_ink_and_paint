585351a7da8021bfc6a22189fc05458bb010472e
Joe Fernette <joefernette@Joes-MacBook-Pro.local>
2021-05-07 13:28:42 -0400

    add form to html

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const ContactForm = () => {
   return (
-    <form name="contact v1" method="POST" data-netlify="true" onSubmit="submit">
-      <input type="hidden" name="form-name" value="contact v1"/>
+    <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+      <input type="hidden" name="form-name" value="contact" />
       <div className="fields">
         <div className="field half">
           <input type="text" name="name" placeholder="Name" />
@@ -29,9 +29,6 @@ export const ContactForm = () => {
             rows="10"
             placeholder="Upload File"
           />
-        </div>
-        <div className="field">
-          <div data-netlify-recaptcha="true"></div>
         </div>
       </div>
       <ul class="actions">
