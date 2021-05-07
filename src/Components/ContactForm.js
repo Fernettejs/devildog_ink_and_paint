a1df914011aa1bd95a5a9299126275afc9730ae7
Joe Fernette <joefernette@Joes-MacBook-Pro.local>
2021-05-07 13:36:04 -0400

    change textarea name to match html

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -13,7 +13,7 @@ export const ContactForm = () => {
         </div>
         <div className="field">
           <textarea
-            name="comments"
+            name="message"
             id="message"
             cols="60"
             rows="10"
