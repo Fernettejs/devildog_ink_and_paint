8cb86bbe2acd8e0550676294312418ce45f4a11f
Joe Fernette <joefernette@Joes-MacBook-Pro.local>
2021-05-07 22:11:33 -0400

    change styling of outlines and fix about page

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
         <h1>DEVILDOG INK & PAINT</h1>
         <h4>Custom Art Studio</h4>
         <button className="banner-btn">
-          <a href="#contact" className="btn">
+          <a href="/#contact" className="btn">
             Get some shit done
           </a>
         </button>
